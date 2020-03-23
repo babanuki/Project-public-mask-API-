87e36e400f4884dc33ad1621fd0c96ef06de8a64
babanuki <52341660+babanuki@users.noreply.github.com>
2020-03-23 18:45:55 +0900

    Update maskAPI.py

--- maskAPI.py
+++ maskAPI.py
@@ -7,14 +7,33 @@ url="https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1"
 print("마스크의 재고가 남아 있는 판매처의 정보가 출력됩니다.")
 print("주소를 입력해주세요. (시군구의 정보가 필요합니다.)")
 
-Inp=input()
+Inp1=input()
 
-r=requests.get(url+"/storesByAddr/json?address="+Inp)
+print("혹시 동 혹은 도로명에 대한 정보가 있다면, 해당 정보도 반영해드리겠습니다.")
+
+Inp2=input()
+
+r=requests.get(url+"/storesByAddr/json?address="+Inp1)
 sales=json.loads(r.text)["stores"]
 
-for s in sales:
+cnt=0
+
+for s in sales:    
     if ("remain_stat" not in s):
+        continue
+
+    if (Inp2 not in s["addr"]):
         continue
     
     if (s["remain_stat"]!="empty") and (s["remain_stat"]!="break"):
-        print(s)
+        cnt+=1
+
+        print(str(cnt)+")")
+        
+        for elem in s:
+            print(elem + " : " + str(s.get(elem)))
+
+        print()
+
+if cnt==0:
+    print("주소 정보가 잘못되었거나, 혹은 마스크 재고가 남은 판매처가 없습니다.")
